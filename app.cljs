a4368f74a318ec7fdcddd3f6f6d31f2850337b2b
Oliver Godby <maleghast@gmail.com>
2022-04-24 19:28:25 +0100

    adding functionality to update birthday gift for person

--- app.cljs
+++ app.cljs
@@ -52,9 +52,57 @@
                    "SELECT * FROM people AS p, gift_ideas AS g WHERE day=? AND month=? AND p.personID=g.personID")
           b_resp (.all b_query day month)
           b_res (js->clj b_resp :keywordize-keys true)]
-         (run! (fn [{:keys [name gift_idea]}]
-           (println "It's" (str name "'s") "Birthday today and they want a" (str gift_idea) "🏆")) b_res)))
+    (run! (fn [{:keys [name gift_idea]}]
+            (println "It's" (str name "'s") "Birthday today and they want a" (str gift_idea) "🏆")) b_res)))
+
+(defn get-people
+  "Function to retrieve the People in the Database"
+  []
+  (p/let [u_query (.prepare db "SELECT * FROM people")
+          u_resp (.all u_query)
+          u_res (js->clj u_resp :keywordize-keys true)
+          res (reduce (fn [acc coll]
+                        (conj acc [(:personID coll) (:name coll)]))
+                      []
+                      u_res)]
+         res))
+
+(def update-questions
+  (p/let [people (get-people)]
+         (clj->js [{:name "personID"
+                    :type "list"
+                    :message "Whose Birthday Gift do you want to update?"
+                    :choices (reduce
+                              (fn
+                                [acc coll]
+                                (conj
+                                 acc
+                                 {:name (last coll)
+                                  :value (first coll)}))
+                              []
+                              people)}
+                   {:name "gift-idea"
+                    :type "input"
+                    :message "What would you like to get for them as a gift?"}])))
+
+(defn update-birthday-gift
+  "Function to write update to Birthday Gift Idea to Database"
+  [personID gift-idea]
+  (p/let [up_query (.prepare db "INSERT INTO gift_ideas(personID, gift_idea) VALUES (?,?)")
+          up_resp (.run up_query name personID gift-idea)
+          up_id (.-lastInsertRowid up_resp)
+          res (if (= 1 up_id) "Success!" "Something went wrong...")]
+         res))
+
+(defn update-birthday-entry
+  "Function to update Birthday Gift Entry in DB"
+  []
+  (p/let [_answers (inquirer/prompt update-questions)
+          answers (js->clj _answers :keywordize-keys true)
+          {:keys [personID gift-idea]} answers]
+    (update-birthday-gift personID gift-idea)))
 
 (cond
   (= (first *command-line-args*) "list") (list-birthdays)
+  (= (first *command-line-args*) "update") (update-birthday-entry)
   :else (create-birthday-entry))
