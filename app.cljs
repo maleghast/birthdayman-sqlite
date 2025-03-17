2d7fe9c62ea92e97a4b1af03e567aa61c17b6753
Clifford Oliver Godby <maleghast@gmail.com>
2025-03-17 20:31:10 +0000

    Added figlet banner to all functions

--- app.cljs
+++ app.cljs
@@ -13,6 +13,36 @@
 ;;; Defining cmd-line args for use via index.mjs
 (def cmd-line-args (not-empty (js->clj (.slice js/process.argv 2))))
 
+(defn script-loc
+  "Function to get the Script Location"
+  []
+  (let [script (path/resolve nbb.core/*file*)]
+    (-> script
+        (s/split #"/")
+        drop-last
+        (->>
+         (interleave (repeat "/")))
+        rest
+        rest
+        s/join)))
+
+(def db (sql. (str (script-loc) "/db/birthdays.db")))
+
+(defn get-banner
+  [banner-message]
+  (figlet/textSync banner-message))
+
+(def top-div
+  "***************************************************************************************************************")
+
+(def hlp-msg (str (fs/readFileSync (str
+                                    (script-loc)
+                                    "/help.txt"))))
+
+(def inv-msg (str (fs/readFileSync (str
+                                    (script-loc)
+                                    "/invalid.txt"))))
+
 (def questions (clj->js [{:name "name"
                           :type "input"
                           :message "Whose Birthday do you want to store?"}
@@ -35,21 +65,6 @@
                           :message "What shall you get for them?"
                           :default "Beats the shit outta me!"}]))
 
-(defn script-loc
-  "Function to get the Script Location"
-  []
-  (let [script (path/resolve nbb.core/*file*)]
-    (-> script
-        (s/split #"/")
-        drop-last
-        (->>
-         (interleave (repeat "/")))
-        rest
-        rest
-        s/join)))
-
-(def db (sql. (str (script-loc) "/db/birthdays.db")))
-
 (defn write-birthday
   "Function to persist a Birthday Record"
   [name day month year gift-idea]
@@ -68,11 +83,19 @@
 
 (defn create-birthday-entry
   "Function to store Birthday Entry in DB"
-  []
+  [banner] 
+  (println top-div)
+  (println banner)
+  (println top-div)
   (p/let [_answers (inquirer/prompt questions)
           answers (js->clj _answers :keywordize-keys true)
           {:keys [name day month year gift-idea]} answers]
     (write-birthday name day month year gift-idea)))
+
+(defn build-create-birthday-entry
+  []
+  (p/let [banner-text (get-banner "BIRTHDAYMAN - SQLITE")]
+    (create-birthday-entry banner-text)))
 
 (defn make-birthday-message
   "Function to build a birthday message"
@@ -94,7 +117,10 @@
 
 (defn list-birthdays
   "Function to retrieve birthdays"
-  []
+  [banner]
+  (println top-div)
+  (println banner)
+  (println top-div)
   (p/let [month (.format (moment) "MMMM")
           day (.date (moment))
           b_query (.prepare
@@ -124,6 +150,11 @@
            (println "No Birthdays Today")
            (console/table (clj->js b_mesgs)))))
 
+(defn build-list-birthdays
+  []
+  (p/let [banner-text (get-banner "BIRTHDAYMAN - SQLITE")]
+    (list-birthdays banner-text)))
+
 (defn get-people
   "Function to retrieve the People in the Database"
   []
@@ -134,9 +165,17 @@
 
 (defn list-people
   "Function to output all of the people in the DB on the command line"
-  []
+  [banner]
+  (println top-div)
+  (println banner)
+  (println top-div)
   (let [users (get-people)]
     (console/table (clj->js users))))
+
+(defn build-list-people
+  []
+  (p/let [banner-text (get-banner "BIRTHDAYMAN - SQLITE")]
+    (list-people banner-text)))
 
 (defn get-people-choices
   "Function to get realised choices from get-people"
@@ -173,11 +212,19 @@
 
 (defn update-birthday-entry
   "Function to update Birthday Gift Entry in DB"
-  []
+  [banner]
+  (println top-div)
+  (println banner)
+  (println top-div)
   (p/let [_answers (inquirer/prompt update-questions)
           answers (js->clj _answers :keywordize-keys true)
           {:keys [personID gift-idea]} answers]
     (update-birthday-gift personID gift-idea)))
+
+(defn build-update-birthday-entry
+  []
+  (p/let [banner-text (get-banner "BIRTHDAYMAN - SQLITE")]
+    (update-birthday-entry banner-text)))
 
 (def delete-questions
   (let [choices (get-people-choices)]
@@ -199,15 +246,26 @@
 (defn delete-birthday-entry
   "Function to delete Birthday Entries all together (manage duplicates,
    un-friend people etc.)"
-  []
+  [banner]
+  (println top-div)
+  (println banner)
+  (println top-div)
   (p/let [_answers (inquirer/prompt delete-questions)
           answers (js->clj _answers :keywordize-keys true)
           {:keys [:personID]} answers]
          (delete-birthday personID)))
 
+(defn build-delete-birthday-entry
+  []
+  (p/let [banner-text (get-banner "BIRTHDAYMNA - SQLITE")]
+    (delete-birthday-entry banner-text)))
+
 (defn search-birthdays-by-day
   "Function to search for Birthdays on a specific day"
-  [day month]
+  [day month banner]
+  (println top-div)
+  (println banner)
+  (println top-div)
   (let [s_query (.prepare db "SELECT * FROM 
                               people AS p, gift_ideas AS g 
                               WHERE day=? 
@@ -234,9 +292,17 @@
       (println "No Birthdays on" (str day " " month))
       (console/table (clj->js s_mesgs)))))
 
+(defn build-search-birthdays-by-day
+  [day month]
+  (p/let [banner-text (get-banner "BIRTHDAYMAN - SQLITE")]
+    (search-birthdays-by-day day month banner-text)))
+
 (defn search-birthdays-by-month
   "Function to search for Birthdays in a specific month"
-  [month]
+  [month banner]
+  (println top-div)
+  (println banner)
+  (println top-div)
   (let [s_query (.prepare db "SELECT * FROM 
                               people AS p, gift_ideas AS g 
                               WHERE month=? 
@@ -261,40 +327,37 @@
       (println "No Birthdays in" (str month))
       (console/table (clj->js s_mesgs)))))
 
-(defn get-banner
-  [banner-message] 
-   (figlet/textSync banner-message))
-
+(defn build-search-birthdays-by-month
+  [month]
+  (p/let [banner-text (get-banner "BIRTHDAYMAN - SQLITE")]
+    (search-birthdays-by-month month banner-text)))
 
 (defn help-message
   "Function to display help and other on-invocation messages"
   [mode banner]
-  (println (str banner))
+  (println top-div)
+  (println banner)
   (cond
-    (= mode "help") (println (str (fs/readFileSync (str
-                                                    (script-loc)
-                                                    "/help.txt"))))
-    (= mode "invalid") (println (str (fs/readFileSync (str
-                                                       (script-loc)
-                                                       "/invalid.txt"))))
+    (= mode "help") (println hlp-msg)
+    (= mode "invalid") (println inv-msg)
     :else (println "Unknown Help Mode")))
 
-(defn help-message-builder
+(defn build-help-message
   [mode]
   (p/let [banner-txt (get-banner "BIRTHDAYMAN - SQLITE")]
     (help-message mode banner-txt)))
 
 (cond
-  (= (first cmd-line-args) "list") (list-birthdays)
-  (= (first cmd-line-args) "list-people") (list-people)
-  (= (first cmd-line-args) "update") (update-birthday-entry)
-  (= (first cmd-line-args) "delete") (delete-birthday-entry)
-  (= (first cmd-line-args) "search-day") (search-birthdays-by-day
+  (= (first cmd-line-args) "list") (build-list-birthdays)
+  (= (first cmd-line-args) "list-people") (build-list-people)
+  (= (first cmd-line-args) "update") (build-update-birthday-entry)
+  (= (first cmd-line-args) "delete") (build-delete-birthday-entry)
+  (= (first cmd-line-args) "search-day") (build-search-birthdays-by-day
                                                 (second cmd-line-args)
                                                 (last cmd-line-args))
-  (= (first cmd-line-args) "search-month") (search-birthdays-by-month
+  (= (first cmd-line-args) "search-month") (build-search-birthdays-by-month
                                                   (last cmd-line-args))
-  (= (first cmd-line-args) "help") (help-message-builder "help")
+  (= (first cmd-line-args) "help") (build-help-message "help")
   :else (if (= 0 (count cmd-line-args))
-          (create-birthday-entry)
-          (help-message-builder "invalid")))
+          (build-create-birthday-entry)
+          (build-help-message "invalid")))
